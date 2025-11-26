d171469c1087353f2fda1edb1ba4032162280b42
Jensen1109 <rodriguezjensen11@gmail.com>
2025-11-26 09:13:28 -0500

    ejer 4 caracterisitcas de celular

---
+++ js/app4.js
@@ -0,0 +1,15 @@
+// Características de un celular: Cree un objeto llamado celular con propiedades:
+// marca, modelo, año y estado. Muestre cada propiedad en consola de manera
+// individual.
+
+const celular = {
+    "marca": "Apple",
+    "modelo": "iPhone 17 Pro Max",
+    "año": 2025,
+    "estado": "Nuevo"
+};
+
+console.log(`Marca: ${celular.marca}`);
+console.log(`Modelo: ${celular.modelo}`);
+console.log(`Año: ${celular.año}`);
+console.log(`Estado: ${celular.estado}`);
