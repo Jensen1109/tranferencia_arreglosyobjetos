a851b0f052b349e832978111ea19fd13f61a034f
Jensen1109 <rodriguezjensen11@gmail.com>
2025-11-26 09:16:41 -0500

    se realizo el ejer 8donde se realizo regirstro de un libro

---
+++ js/app8.js
@@ -0,0 +1,12 @@
+// Registro de un libro: Cree un objeto llamado libro con las propiedades: título, autor,
+// número de páginas y género. Luego imprima un mensaje descriptivo usando estas
+// propiedades, por ejemplo:
+// "El libro '___' del autor ___ pertenece al género ___."
+
+const libro = {
+    "titulo": "Cien Años de Soledad",
+    "autor": "Gabriel García Márquez",
+    "numero de paginas": 417,
+    "genero": "Realismo Mágico"
+};
+console.log(`El libro '${libro.titulo}' del autor ${libro.autor} pertenece al genero ${libro.genero}.`);
